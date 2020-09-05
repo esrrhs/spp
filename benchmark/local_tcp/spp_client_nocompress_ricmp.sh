5f3d238b0a26e7506f99d8e3f847c0b23f4a943a
esrrhs <esrrhs@163.com>
2020-09-05 20:01:35 +0800

    add

--- benchmark/local_tcp/spp_client_nocompress_ricmp.sh
+++ benchmark/local_tcp/spp_client_nocompress_ricmp.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-../../spp -name "test" -type proxy_client -server :8888 -fromaddr :8855 -toaddr :8844 -proxyproto tcp -proto ricmp -compress 0 -encrypt=""
+../../spp -name "test" -type proxy_client -server 127.0.0.1 -fromaddr :8855 -toaddr :8844 -proxyproto tcp -proto ricmp -compress 0 -encrypt=""
